ce9a67cce9f585e47a5398c59fb4f23ee1334a60
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-28 21:22:54 +0200

    headers

--- src/components/homePage/header/Authentication.tsx
+++ src/components/homePage/header/Authentication.tsx
@@ -1,15 +1,30 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { useSession, signIn, signOut } from "next-auth/react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+  
 export const Authentication = () => {
   const { data: session } = useSession()
   
   return <>
     {!session ? (
         <div className="flex items-center gap-4">
-            <Button variant="ghost" className="border-gray-300 border" onClick={() => signIn()}>Sign in</Button>
-            <Button variant="blue">Sign up</Button>
+            <Button variant="ghost" className="border-gray-300 border font-bold" onClick={() => signIn()}>Sign in</Button>
+            <Button variant="blue" className="cursor-not-allowed">Sign up</Button>
         </div>
-    ):null}  
+    ): (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <img src={session.user?.image || ""} alt="user avatar" className="w-9 h-9 rounded-full" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )}  
   </>
 }
--- src/components/homePage/header/ThemeController.tsx
+++ src/components/homePage/header/ThemeController.tsx
@@ -6,7 +6,7 @@ export const ThemeController = () => {
 
   {/* sun icon */}
   <svg
-    className="swap-off h-8 w-8 fill-current"
+    className="swap-off h-7 w-7 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24">
     <path
@@ -15,7 +15,7 @@ export const ThemeController = () => {
 
   {/* moon icon */}
   <svg
-    className="swap-on h-8 w-8 fill-current"
+    className="swap-on h-7 w-7 fill-current"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24">
     <path
