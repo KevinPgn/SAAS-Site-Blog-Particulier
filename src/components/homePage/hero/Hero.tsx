6345cb4b62f5a925e49c7be3cd52c7dffd19c8fd
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-28 21:48:56 +0200

    hero section

--- src/components/homePage/hero/Hero.tsx
+++ src/components/homePage/hero/Hero.tsx
@@ -2,19 +2,19 @@ import { Button } from "@/components/ui/button"
 
 export const Hero = () => {
   return (
-    <section className="max-w-[1500px] mb-10 overflow-hidden flex flex-col items-center justify-center mx-auto mt-10 relative">
+    <section className="max-w-[1500px] mb-10 overflow-hidden flex flex-col items-center justify-center mx-auto mt-10 relative px-4 sm:px-6 lg:px-8">
       {/* Ajout de l'effet de lueur */}
       
       {/* Contenu existant */}
-      <div className="p-1 rounded-full text-blue-500 w-fit mx-auto text-md font-semibold bg-[#E8EAFC] px-5">
+      <div className="p-1 rounded-full text-blue-500 w-fit mx-auto text-xs sm:text-sm md:text-md font-semibold bg-[#E8EAFC] px-3 sm:px-5">
         <span>Ultimate Blogging SaaS for Startups</span>
       </div>
 
-      <h1 className="text-center text-8xl max-md:text-4xl w-[60%] max-md:w-full mt-10 font-medium">Setup your Blog <span className="text-blue-500 font-semibold">in Minutes!</span></h1>
-      <p className="text-center text-xl max-md:text-sm w-[45%] max-md:w-full text-gray-500 mt-5">Setting up your blog is hard and time consuming. We make it easy for you to create a blog in minutes</p>
-      <div className="flex items-center gap-4 mt-10">
-        <Button className="border-gray-300 border font-bold p-5" variant="ghost">Get Started</Button>
-        <Button variant="blue" className="p-5">Try for Free</Button>
+      <h1 className="text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl w-full sm:w-[80%] md:w-[70%] lg:w-[70%] mt-6 sm:mt-10 font-medium">Setup your Blog <span className="text-blue-500 font-semibold">in Minutes!</span></h1>
+      <p className="text-center text-base sm:text-lg md:text-xl w-full sm:w-[70%] md:w-[60%] lg:w-[45%] text-gray-500 mt-3 sm:mt-5">Setting up your blog is hard and time consuming. We make it easy for you to create a blog in minutes</p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-6 sm:mt-10">
+        <Button className="border-gray-300 border font-bold p-4 sm:p-5 w-full sm:w-auto" variant="ghost">Get Started</Button>
+        <Button variant="blue" className="p-4 sm:p-5 w-full sm:w-auto">Try for Free</Button>
       </div>
 
       <div className="relative w-full mt-10">
