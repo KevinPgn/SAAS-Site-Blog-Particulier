d470290d342266a64dd4f4aa592e785de19a6778
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-29 01:19:20 +0200

    partners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Headers } from "@/components/homePage/header/Headers";
 import { Hero } from "@/components/homePage/hero/Hero";
+import { Partnaires } from "@/components/homePage/partnaires/Partnaires";
 export default function Home() {
   return (
   <>
     <Headers />
     <Hero />
+    <Partnaires />
   </>
   );
 }
---
+++ src/components/homePage/partnaires/Partnaires.tsx
@@ -0,0 +1,15 @@
+//img : prisma.svg, next.svg, auth.png
+
+export const Partnaires = () => {
+  return <section className="max-w-[1500px] mx-auto px-4 sm:px-6 lg:px-8 py-10">
+    <h2 className="text-2xl font-bold text-center mb-10">Trusted by</h2>
+    <div className="flex flex-wrap justify-between items-center gap-4">
+      <img src="/prisma.svg" alt="prisma" className="w-32 h-32 cursor-pointer hover:scale-110 transition-all duration-300" />
+      <img src="/next.svg" alt="next" className="w-32 h-32 cursor-pointer hover:scale-110 transition-all duration-300" />
+      <img src="/auth.png" alt="auth" className="w-24 h-24 cursor-pointer hover:scale-110 transition-all duration-300" />
+      <img src="/prisma.svg" alt="prisma" className="w-32 h-32 cursor-pointer hover:scale-110 transition-all duration-300" />
+      <img src="/next.svg" alt="next" className="w-32 h-32 cursor-pointer hover:scale-110 transition-all duration-300" />
+      <img src="/auth.png" alt="auth" className="w-24 h-24 cursor-pointer hover:scale-110 transition-all duration-300" />
+    </div>
+  </section>
+}
