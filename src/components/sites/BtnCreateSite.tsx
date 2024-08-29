3e5f5447e3b649fae9d3a9376890e57bd6aa76b7
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-29 14:05:23 +0200

    btn sites create

--- src/components/sites/BtnCreateSite.tsx
+++ src/components/sites/BtnCreateSite.tsx
@@ -1,8 +1,17 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Plus } from "lucide-react"
+import { useRouter } from "next/navigation"
+
 export const BtnCreateSite = () => {
+  const router = useRouter()
   return <>
-    <Button variant="blue" className="flex items-center gap-2">
+    <Button
+      variant="blue"
+      className="flex items-center gap-2"
+      onClick={() => router.push('/profile/sites/create')}
+    >
       <Plus size={16} />
       Create Site
     </Button>
