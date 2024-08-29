c7a6cf0cbab5d5b1802745c4b072f067cf24c352
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-29 13:31:44 +0200

    sidebar

--- src/components/profile/EnTeteProfile.tsx
+++ src/components/profile/EnTeteProfile.tsx
@@ -2,12 +2,11 @@ import Link from "next/link"
 import Image from "next/image"
 import { Authentication } from "../homePage/header/Authentication"
 import { ThemeController } from "../homePage/header/ThemeController"
-import {useTheme} from "next-themes"
 
 export const EnTeteProfile = () => {
   return <header className="h-16 border-b border-gray-300">
     <nav className="flex items-center justify-between h-full">
-        <Link href="/" className="flex items-center h-full w-[250px] border-r border-gray-300 gap-2 px-3">
+        <Link href="/" className="flex items-center h-full w-[290px] border-r border-gray-300 gap-2 px-5">
             <Image src="/logo.svg" alt="logo" width={40} height={40} />
             <h1 className="text-2xl font-extrabold">Blog<span className="text-blue-500">Creator</span></h1>
         </Link>
--- src/components/profile/SidebarProfile.tsx
+++ src/components/profile/SidebarProfile.tsx
@@ -1,5 +1,22 @@
-export const SidebarProfile = () => {
-  return <div className="w-[250px] h-full border-r border-gray-300">
+"use client"
+import {Home, Globe, DollarSign} from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
+export const SidebarProfile = () => {
+  const pathname = usePathname()
+  return <div className="w-[290px] p-3 flex flex-col gap-2 h-full border-r border-gray-300">
+    <Link href="/profile/dashboard" className={`flex items-center gap-2 px-5 py-3 rounded-lg hover:bg-gray-100 duration-75 ${pathname === '/profile/dashboard' ? 'bg-gray-100 text-blue-500' : ''}`}>
+      <Home size={20} />
+      <span className="font-semibold">Dashboard</span>
+    </Link>
+    <Link href="/profile/sites" className={`flex items-center gap-2 px-5 py-3 rounded-lg hover:bg-gray-100 hover:text-blue-500 duration-75 ${pathname === '/profile/sites' ? 'bg-gray-100 text-blue-500' : ''}`}>
+      <Globe size={20} />
+      <span className="font-semibold">Sites</span>
+    </Link>
+    <Link href="/profile/pricing" className={`flex items-center gap-2 px-5 py-3 rounded-lg hover:bg-gray-100 hover:text-blue-500 duration-75 ${pathname === '/profile/pricing' ? 'bg-gray-100 text-blue-500' : ''}`}>
+      <DollarSign size={20} />
+      <span className="font-semibold">Pricing</span>
+    </Link>
   </div>
 }
