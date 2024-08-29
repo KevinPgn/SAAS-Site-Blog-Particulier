3786c75ca8b9a8889340dc468465cad38839ff21
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-29 14:40:28 +0200

    developer

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -27,23 +27,16 @@ export const Dashboard = ({ sites }: { sites: any }) => {
     </div>
 
     <div className="mt-4 flex max-xl:justify-center flex-wrap gap-5">
-        <div className="w-[400px] shadow-lg rounded-lg">
-            <div className="w-full h-[200px] rounded-t-lg bg-purple-800"></div>
-            <div className="p-4 py-6 px-7">
-                <h2 className="text-2xl font-bold">KevinPgn</h2>
-                <p className="text-gray-500 mt-2">Hey, I'm KevinPgn, I'm a web developer and I love to code</p>
-                <Button variant="blue" className="mt-4 w-full">View Articles</Button>
+        {sites.map((site: any) => (
+            <div key={site.id} className="w-[400px] shadow-lg rounded-lg">
+                <img src={site.imageUrl} loading="lazy" alt={site.name} className="w-full h-[200px] object-cover rounded-t-lg" />
+                <div className="p-4 py-6 px-7">
+                    <h2 className="text-2xl font-bold">{site.name}</h2>
+                    <p className="text-gray-500 mt-2">{site.description}</p>
+                    <Button variant="blue" className="mt-4 w-full">View Articles</Button>
+                </div>
             </div>
-        </div>
-
-        <div className="w-[400px] shadow-lg rounded-lg">
-            <div className="w-full h-[200px] rounded-t-lg bg-blue-800"></div>
-            <div className="p-4 py-6 px-7">
-                <h2 className="text-2xl font-bold">KevinPgn Blog</h2>
-                <p className="text-gray-500 mt-2">Hey, this is a super blog about web development</p>
-                <Button variant="blue" className="mt-4 w-full">View Articles</Button>
-            </div>
-        </div>
+        ))}
     </div>
 
     <h2 className="text-2xl font-bold mt-10 px-2">Recents Articles</h2>
--- src/components/sites/Sites.tsx
+++ src/components/sites/Sites.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../ui/button"
 import { BtnCreateSite } from "./BtnCreateSite"
 
 export const Sites = ({ sites }: { sites: any }) => {
@@ -16,6 +17,18 @@ export const Sites = ({ sites }: { sites: any }) => {
   return <div className="w-full">
     <div className="flex justify-end">
       <BtnCreateSite />
+    </div>
+    <div className="mt-4 flex max-xl:justify-center flex-wrap gap-5">
+        {sites.map((site: any) => (
+            <div key={site.id} className="w-[400px] shadow-lg rounded-lg">
+                <img src={site.imageUrl} loading="lazy" alt={site.name} className="w-full h-[200px] object-cover rounded-t-lg" />
+                <div className="p-4 py-6 px-7">
+                    <h2 className="text-2xl font-bold">{site.name}</h2>
+                    <p className="text-gray-500 mt-2">{site.description}</p>
+                    <Button variant="blue" className="mt-4 w-full">View Articles</Button>
+                </div>
+            </div>
+        ))}
     </div>
   </div>
 }
