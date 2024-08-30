b0d56b4d44718ba1271fb6021d951a3672c5fc42
TeruBW <kevin.pigeon2810@gmail.com>
2024-08-30 13:03:10 +0200

    responsive

--- src/components/dashboard/siteDetail/ArticlesBtn.tsx
+++ src/components/dashboard/siteDetail/ArticlesBtn.tsx
@@ -3,11 +3,11 @@ import { Button } from "@/components/ui/button"
 import {Plus, Settings, ExternalLink} from 'lucide-react'
 
 export const ArticlesBtn = () => {
-  return <>
-     <div className="w-full justify-end px-5 flex items-center gap-5">
+  return <div className="w-full">
+     <div className="w-full justify-end flex items-center gap-5">
         <Button variant='outline' className="gap-2 text-black"><ExternalLink size={18} /> View Blog</Button>
         <Button variant='outline' className="gap-2 text-black"><Settings size={18} /> Settings</Button>
         <Button variant='blue' className="gap-2"><Plus size={18} /> Create Article</Button>
     </div>
-  </>
+  </div>
 }
